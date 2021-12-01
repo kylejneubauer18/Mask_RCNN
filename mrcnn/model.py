070e53de205c80cea95ee3c23ee5fc01f055a3c6
kylejneubauer18 <86132200+kylejneubauer18@users.noreply.github.com>
2021-12-01 14:33:32 -0600

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -20,7 +20,7 @@ import tensorflow as tf
 import keras
 import keras.backend as K
 import keras.layers as KL
-import keras.engine as KE
+from tensorflow.keras.layers import Layer, InputSpec as KE
 import keras.models as KM
 
 from mrcnn import utils
